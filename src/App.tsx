e88b0bcd942bc3e1e608b66b50761a8751ed554d
Rastilan <christoperbhall@gmail.com>
2025-07-26 00:59:58 -0400

    updating taskbar layout to prepare for inclusion of components

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,22 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-
   return (
     <>
-      <main className='h-[95vh] min-w-screen bg-gray-300'>screen</main>
-      <div className='h-[5vh] min-w-screen bg-amber-400'>taskbar</div>
+      <main className="min-h-screen w-screen bg-gray-300">screen</main>
+
+      <div className="fixed bottom-0 z-10 h-[5vh] w-screen bg-amber-400 flex items-center justify-between px-4">
+        {/* Left side of task bar*/}
+        <section className="text-left">Weather App</section>
+
+        {/* Center of task bar */}
+        <section className="absolute left-1/2 transform -translate-x-1/2">
+          Windows Logo - Search - icons
+        </section>
+
+        {/* Right side of task bar */}
+        <section className="text-right">More icons - apps - controls - time</section>
+      </div>
     </>
   )
 }
